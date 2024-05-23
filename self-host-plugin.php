802dc86edb9d293c4fa7cceffc3c9640cfa4f4cb
Hiren Ghoghari <ghogharihiren175@gmail.com>
2024-05-24 00:04:48 +0530

    v1.4.0

--- self-host-plugin.php
+++ self-host-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Self Host Plugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.3.0
+ * Version:           1.4.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Hiren Ghoghari
