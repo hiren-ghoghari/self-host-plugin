7451ce48617fe739901b05f86ebe946912a09bdc
Hiren Ghoghari <ghogharihiren175@gmail.com>
2024-05-24 00:02:47 +0530

    v1.3.0

--- self-host-plugin.php
+++ self-host-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Self Host Plugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Hiren Ghoghari
@@ -26,4 +26,6 @@
 //Set the branch that contains the stable release.
  $myUpdateChecker->setBranch('master');
 
- echo "version 1.2.0";
+add_action( 'wp_head', function(){
+	 echo "version 1.3.0";	
+} );
